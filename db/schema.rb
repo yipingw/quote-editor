ab04fe22b71a84548b01b0dbeecb97f03dfc1f6b
Fenix Wang <fenix.wang@ninjageek.ai>
2025-09-22 16:01:01 +0800

    添加公司引用，更新用户和报价模型，增强身份验证，添加导航栏和主页视图

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_19_090824) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_072117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_19_090824) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,7 +36,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_19_090824) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
